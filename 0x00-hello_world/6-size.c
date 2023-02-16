59f78b2ed7f964e7d98a7e823b2351d140019bc3
Hatim Ousilmaati <hatim.ousilmaati@gmail.com>
2023-02-16 14:27:35 -0800

    Updated 0x00-hello_world

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,10 +2,10 @@
 /**
  * main - entry
  *
- * return: 0
+ * Return: 0
  */
 int main(void)
 {
-	printf("Size of a char: %lu byte(s)\nSize of an int: %lu byte(s)\nSize of a long int: %lu byte(s)\nSize of a long long int: %lu byte(s)\nSize of a float: %lu byte(s)\n",sizeof(char),sizeof(int),sizeof(long),sizeof(long long),sizeof(float));
+	printf("Size of a char: %lu byte(s)\nSize of an int: %lu byte(s)\nSize of a long int: %lu byte(s)\nSize of a long long int: %lu byte(s)\nSize of a float: %lu byte(s)\n", sizeof(char), sizeof(int), sizeof(long), sizeof(long long), sizeof(float));
 	return (0);
 }
