776f0076ef62afc97541af90b6736103b130609d
Hatim Ousilmaati <hatim.ousilmaati@gmail.com>
2023-02-16 15:27:31 -0800

    Updated 0x00-hello_world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
  * main - main funct
  *
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-	perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 33);
 	return (1);
 }
