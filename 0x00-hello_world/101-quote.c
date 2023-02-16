c103678c2f5e9f48b27dcb712a65b374dc5dc06a
Hatim Ousilmaati <hatim.ousilmaati@gmail.com>
2023-02-16 15:35:39 -0800

    Updated 0x00-hello_world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,6 @@
  */
 int main(void)
 {
-	char ch[] ="and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-	fwrite(ch, 1, 59, stderr);
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 1, 59, stderr);
 	return (1);
 }
