a827f545dc0842bcfef126336456933383d72726
Hatim Ousilmaati <hatim.ousilmaati@gmail.com>
2023-02-17 14:49:25 -0800

    Updated 0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - default entry
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char i;
+	for (i = 'a'; i != 'z' + 1; i++)
+	{
+		if (i != 'e' && i != 'q')
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
