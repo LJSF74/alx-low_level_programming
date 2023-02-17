e02c6c168a0d30889d18631ae9d2e0cb4aa4bdf2
Hatim Ousilmaati <hatim.ousilmaati@gmail.com>
2023-02-17 14:43:18 -0800

    Updated 0x01-variables_if_else_while

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,17 +6,17 @@
  */
 int main(void)
 {
-        char i;
+	char i;
 
-        for (i = 'a'; i != 'z' + 1 ; i++)
-        {
-                putchar(i);
-        }
-	 for (i = 'A'; i != 'Z' + 1 ; i++)
-        {
-                putchar(i);
-        }
-        putchar('\n');
-        return (0);
+	for (i = 'a'; i != 'z' + 1 ; i++)
+	{
+		putchar(i);
+	}
+	for (i = 'A'; i != 'Z' + 1 ; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
 }
 
