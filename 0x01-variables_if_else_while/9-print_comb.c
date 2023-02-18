e3873e070dd6e20f51c20b498601ff98f8037dba
Hatim Ousilmaati <hatim.ousilmaati@gmail.com>
2023-02-17 17:14:02 -0800

    Updated 0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - default entry
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+		if (i < '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
