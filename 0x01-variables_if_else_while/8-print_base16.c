167d67adb728282303276b3e84e8e2e47b884303
Hatim Ousilmaati <hatim.ousilmaati@gmail.com>
2023-02-17 17:10:19 -0800

    Updated 0x01-variables_if_else_while

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 /**
  * main - default entry
- * 
+ *
  * Return: 0
  */
-int main(void) {
+int main(void)
+{
 	char i;
+
 	for (i = '0'; i <= '9'; i++)
 	{
 		putchar(i);
 	}
-	for (i ='a'; i <= 'f'; i++)
+	for (i = 'a'; i <= 'f'; i++)
 	{
 		putchar(i);
 	}
