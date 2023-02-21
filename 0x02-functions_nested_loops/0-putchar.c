0e7ba5353b7f516e50dfd491197bd3e2bc07b79e
Hatim Ousilmaati <hatim.ousilmaati@gmail.com>
2023-02-21 13:33:00 -0800

    Updated 0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+/**
+ * main - default entry
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char c[] = { '_', 'p', 'u', 't', 'c', 'h', 'a', 'r' };
+	int i;
+	
+	for( i = 0; i < 8; i++ ){
+		_putchar(c[i]);
+	}
+	return (0);
+}
