b2592ed2b9a44812cade4f97bb7e7be9c58eb390
Hatim Ousilmaati <hatim.ousilmaati@gmail.com>
2023-02-21 15:00:09 -0800

    Updated 0x02-functions_nested_loops

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,16 +1,24 @@
 #include "main.h"
 /**
  * _islower - returns 1 if lowercase and 0 if not
+ *
+ * Return: 1
+ * Return: 0
  */
 int _islower(int c)
 {
 	char i;
+	int j;
 
+	for (j = 0 ; j <= 20 ; i++)
+	{
+		if (c == j)
+			return (1);
+	}
 	for (i = 'A' ; i <= 'Z' ; i++)
 	{
-		if (c == i){
+		if (c == i)
 			return (0);
-		}
 	}
 	return (1);
 }
