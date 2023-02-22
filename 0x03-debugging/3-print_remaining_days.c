19b763443b6e574f8754cd0b2a6603e932fe5b21
Hatim Ousilmaati <hatim.ousilmaati@gmail.com>
2023-02-22 14:21:50 -0800

    Updated 0x03-debugging

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -13,7 +13,7 @@ void print_remaining_days(int month, int day, int year)
 {
 	if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
 	{
-		if (month >= 2 && day >= 60)
+		if (month >= 2 && day >= 61)
 		{
 			day++;
 		}
@@ -23,17 +23,8 @@ void print_remaining_days(int month, int day, int year)
 	}
 	else
 	{
-		
-		if (month == 2 && day == 60)
-		{
-			printf("Day of the year: %d\n", day);
-			printf("Remaining days: %d\n", 366 - day);
-		}
-		else
-		{
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 365 - day);
-		}
 	}
 }
 
