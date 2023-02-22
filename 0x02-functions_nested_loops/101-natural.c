168ddc353d4fddd76459b8d78ec5bae25698b76a
Hatim Ousilmaati <hatim.ousilmaati@gmail.com>
2023-02-21 18:27:54 -0800

    Updated 0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints the sum of the multiples of the number in parameter
+ * 
+ * Return: 0
+ */
+int main(void)
+{
+	int sum = 0;
+
+	for (i = 1024 ; i >= 0 ; i--)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum = sum + i;
+		}
+	}
+	printf("%d", sum);
+	return (0);
+}
