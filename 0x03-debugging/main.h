83ebc37d28cf57403f20501c3a4cffcf3345be94
Hatim Ousilmaati <hatim.ousilmaati@gmail.com>
2023-02-22 13:44:39 -0800

    Updated 0x03-debugging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -11,3 +11,7 @@ void positive_or_negative(int i);
 #include <stdio.h>
 int largest_number(int a, int b, int c);
 #endif
+#ifndef PRNT_REM_DAYS
+#define PRNT_REM_DAYS
+void print_remaining_days(int month, int day, int year);
+#endif
