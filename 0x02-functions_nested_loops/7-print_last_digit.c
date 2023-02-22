1d5764aaa80a767604cd7b04b255f20c7a44edba
Hatim Ousilmaati <hatim.ousilmaati@gmail.com>
2023-02-21 16:38:56 -0800

    Updated 0x02-functions_nested_loops

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,16 +7,6 @@
  */
 int print_last_digit(int n)
 {
-	int last;
-
-	if (n < 0)
-	{
-		last = (-1 * n) % 10;
-	}
-	else
-	{
-		last = n % 10;
-	}
-	printf("%d", last);
+	printf("%d", n % 10);
 	return (0);
 }
