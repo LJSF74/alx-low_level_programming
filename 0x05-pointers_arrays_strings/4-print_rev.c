031be0730749274f8a32b6d2f7e5f656d0405012
Hatim Ousilmaati <hatim.ousilmaati@gmail.com>
2023-02-27 13:33:26 -0800

    Updated 0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,16 +7,16 @@
  */
 void print_rev(char *s)
 {
-	int i = 1;
+	int i = 0;
 
 	while (*(s + i) != '\0')
 	{
 		i++;
 	}
-	while (i >= 0)
+	while (i >= 1)
 	{
-		printf("%c", *(s + i));
 		i--;
+		printf("%c", *(s + i));
 	}
 	printf("\n");
 }
