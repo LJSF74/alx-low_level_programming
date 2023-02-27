ddf7dc3edd36e0b2b9c7b63623d56e56a4916c13
Hatim Ousilmaati <hatim.ousilmaati@gmail.com>
2023-02-27 12:47:59 -0800

    Updated 0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int - swaps value of two int
+ * @a: first in
+ * @b: second int
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int *s;
+
+	*s = *a;
+	*a = *b;
+	*b = *s;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,3 +2,7 @@
 #define RESET_TO_98
 void reset_to_98(int *n);
 #endif
+#ifndef SWAP_INT
+#define SWAP_INT
+void swap_int(int *a, int *b);
+#endif
