c3279bfd4dda1c7129a36e3fdb856f77e3560a7e
Hatim Ousilmaati <hatim.ousilmaati@gmail.com>
2023-03-01 14:49:27 -0800

    Updated 0x06-pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two strings
+ * @dest: new string
+ * @src: source string
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,4 @@
+#ifndef STR_CAT
+#define STR_CAT
+char *_strcat(char *dest, char *src);
+#endif
