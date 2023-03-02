3c653b67b82659b902e07742c8272b796c34456f
Hatim Ousilmaati <hatim.ousilmaati@gmail.com>
2023-03-02 06:08:37 -0800

    Updated 0x06-pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strncat - concatenates n bytes of src to dest
+ * @dest: destination
+ * @src: source
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = strlen(dest);
+	int j = strlen(src);
+	int k = 0;
+
+	while (k < n)
+	{
+		*(dest + i) = *(src + k);
+	}
+	return dest;
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,3 +2,7 @@
 #define STR_CAT
 char *_strcat(char *dest, char *src);
 #endif
+#ifndef STR_NCAT
+#define STR_NCAT
+char *_strncat(char *dest, char *src, int n);
+#endif
