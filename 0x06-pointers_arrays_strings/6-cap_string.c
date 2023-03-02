2852615ee0e6faca43d376e654c406e08d96c62a
Hatim Ousilmaati <hatim.ousilmaati@gmail.com>
2023-03-02 11:14:46 -0800

    Updated 0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -19,7 +19,7 @@ char *cap_string(char *s)
 		{
 			if (*(s + i) == sep[j])
 			{
-				if (*(s + i + 1) >= 'a' && *(s + i + 1) <= 'z' )
+				if (*(s + i + 1) >= 'a' && *(s + i + 1) <= 'z')
 					*(s + i + 1) = *(s + i + 1) - 32;
 			}
 		}
