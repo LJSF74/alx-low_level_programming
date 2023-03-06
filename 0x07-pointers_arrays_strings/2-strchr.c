b6f751728242a0dc6a750e34d3be691de2bbb7f0
Hatim Ousilmaati <hatim.ousilmaati@gmail.com>
2023-03-06 15:58:36 -0800

    Updated 0x07-pointers_arrays_strings

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,23 +8,13 @@
  */
 char *_strchr(char *s, char c)
 {
-	static char charac[sizeof(s)];
+	static char *charac;
 	int i = 0;
-	unsigned long int j;
-	int k = 0;
 
-	while (s[i] != c)
+	while (s[i] != '\0')
 	{
+		ret = strchr(s, c);
 		i++;
-		if (s[i] == '\0')
-		{
-			return (NULL);
-		}
-	}
-	for (j = i ; j < sizeof(s); j++)
-	{
-		charac[k] = s[j];
-		k++;
 	}
 	return (charac);
 }
