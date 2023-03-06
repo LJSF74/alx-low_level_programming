2c22f0f455b7ec2a35658a2bd69a892ac7dcf628
Hatim Ousilmaati <hatim.ousilmaati@gmail.com>
2023-03-06 14:46:55 -0800

    Updated 0x07-pointers_arrays_strings

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memcpy - copy memory area
+ * @dest: copied data
+ * @src: data to be copied
+ * n: num of bytes to copy
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i;
+
+	for (i = 0 ; i < n ; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,3 +2,7 @@
 #define _MEMSET
 char *_memset(char *s, char b, unsigned int n);
 #endif
+#ifndef _MEMCPY
+#define _MEMCPY
+char *_memcpy(char *dest, char *src, unsigned int n);
+#endif
