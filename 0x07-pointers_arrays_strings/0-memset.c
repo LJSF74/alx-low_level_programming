fbb35cc8672c3fb950220fb12248e874e93516c0
Hatim Ousilmaati <hatim.ousilmaati@gmail.com>
2023-03-06 14:37:07 -0800

    Updated 0x07-pointers_arrays_strings

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,7 +4,15 @@
  * @s: pointer to memory area
  * @b: constant byte
  * @n: number of bytes to fill
+ * Return: s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
+	int i;
+
+	for (i = 0 ; i < n ; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
 }
