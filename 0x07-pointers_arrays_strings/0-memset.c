5bd40c8d6a185484c4b8cfe76a807796c072c8c0
Hatim Ousilmaati <hatim.ousilmaati@gmail.com>
2023-03-06 12:29:18 -0800

    Updated 0x07-pointers_arrays_strings

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _memset - fills n bytes of memory with b
+ * @s: pointer to memory area
+ * @b: constant byte
+ * @n: number of bytes to fill
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+}
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,4 @@
+#ifndef _MEMSET
+#define _MEMSET
+char *_memset(char *s, char b, unsigned int n);
+#endif
