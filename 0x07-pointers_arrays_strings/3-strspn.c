3df6572947823b2ec8c6abcce671f0b19c0ac6c7
Hatim Ousilmaati <hatim.ousilmaati@gmail.com>
2023-03-06 16:05:18 -0800

    Updated 0x07-pointers_arrays_strings

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 /**
  * _strspn - counts num of bytes of accept that are in s
  * @s: string
@@ -7,18 +8,5 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned long int i, j;
-	unsigned int n = 0;
-
-	for (i = 0 ; i < sizeof(s) ; i++)
-	{
-		for (j = 0 ; j < sizeof(accept) ; j++)
-		{
-			if (s[i] == accept[j])
-			{
-				n++;
-			}
-		}
-	}
-	return (n);
+	return (strspn(s, accept));
 }
