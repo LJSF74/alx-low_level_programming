986e02e8f0032f9627a8492890b679944ae04c07
Hatim Ousilmaati <hatim.ousilmaati@gmail.com>
2023-03-08 15:59:45 -0800

    Updated 0x08-recursion

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * factorial - prints factorial of n recursively
+ * @n: num
+ * Return: factn
+ */
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * fact(n - 1));
+	}
+}
