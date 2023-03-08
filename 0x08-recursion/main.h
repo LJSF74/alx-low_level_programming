3552eab82f0b7df52a0e8a216b58bd0b79016f17
Hatim Ousilmaati <hatim.ousilmaati@gmail.com>
2023-03-08 08:17:08 -0800

    Updated 0x08-recursion

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,28 @@
+#ifndef PUTS_REC
+#define PUTS_REC
+void _puts_recursion(char *s);
+#endif
+#ifndef PR_REV_REC
+#define PR_REV_REC
+void _print_rev_recursion(char *s);
+#endif
+#ifndef STRLEN_REC
+#define STRLEN_REC
+int _strlen_recursion(char *s);
+#endif
+#ifndef FACTR
+#define FACTR
+int factorial(int n);
+#endif
+#ifndef POW_REC
+#define POW_REC
+int _pow_recursion(int x, int y);
+#endif
+#ifndef SQRT_REC
+#define SQRT_REC
+int _sqrt_recursion(int n);
+#endif
+#ifndef IS_PRM
+#define IS_PRM
+int is_prime_number(int n);
+#endif
