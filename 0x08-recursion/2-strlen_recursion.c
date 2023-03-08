6f413a3b5640563c74a6a9e1f404d25e86ee1d65
Hatim Ousilmaati <hatim.ousilmaati@gmail.com>
2023-03-09 00:25:49 +0100

    Updated 0x08-recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strlen_recursion - returns length of a string recursively
+ * @s: string
+ * Return: l
+ */
+int _strlen_recursion(char *s)
+{
+	static int l = 0;
+	if (*s == '\0')
+	{
+		return (l);
+	}
+	else
+	{
+		l++;
+		_strlen_recursion(s + 1);
+	}
+}
