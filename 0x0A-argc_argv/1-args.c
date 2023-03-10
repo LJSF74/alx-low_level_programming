b08e71922633d81e3feb97aa1c298452c5f7b32e
Hatim Ousilmaati <hatim.ousilmaati@gmail.com>
2023-03-10 14:17:19 -0800

    Updated 0x09-static_libraries

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints the program name
+ * @argc: number of args
+ * @argv: array of args
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d", argc);
+	return (0);
+}
