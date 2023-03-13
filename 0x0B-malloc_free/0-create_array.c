d02db0f708b3e5b5ad4c929e807f1fd7f7022902
Hatim Ousilmaati <hatim.ousilmaati@gmail.com>
2023-03-13 10:53:20 -0700

    Updated 0x0B-malloc_free

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,7 +17,7 @@ char *create_array(unsigned int size, char c)
 	}
 	else
 	{
-		arr = malloc(size * sizeof(char) + 1);
+		arr = malloc(size * sizeof(char));
 		for (i = 0 ; i < size ; i++)
 		{
 			arr[i] = c;
