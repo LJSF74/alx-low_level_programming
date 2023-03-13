679d4b99d3b6721503e496e35bf0884b415a7ba1
Hatim Ousilmaati <hatim.ousilmaati@gmail.com>
2023-03-13 12:48:03 -0700

    Updated 0x0B-malloc_free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,10 +16,27 @@ char *str_concat(char *s1, char *s2)
 	i = 0;
 	if (s1 == NULL && s2 == NULL)
 	{
-		news = '\0';
+		news = malloc(sizeof(char));
+		news[0] = '\0';
 		return (news);
 	}
-	news = malloc(strlen(s1) + strlen(s2) + 1);
+	if (s1 == NULL)
+	{
+		l1 = 0;
+	}
+	else
+	{
+		l1 = strlen(s1);
+	}
+	if (s2 == NULL)
+	{
+		l2 = 0;
+	}
+	else
+	{
+		l2 = strlen(s2);
+	}
+	news = malloc(l1 + l2 + 1);
 	if (news == NULL)
 	{
 		printf("Can't allocate memory");
@@ -27,7 +44,6 @@ char *str_concat(char *s1, char *s2)
 	}
 	if (s1 != NULL)
 	{
-		l1 = strlen(s1);
 		for ( ; i < l1 ; i++)
 		{
 			news[i] = s1[i];
@@ -35,7 +51,6 @@ char *str_concat(char *s1, char *s2)
 	}
 	if (s2 != NULL)
 	{
-		l2 = strlen(s2);
 		for (j = 0 ; j < l2 ; j++)
 		{
 			news[i + j] = s2[j];
