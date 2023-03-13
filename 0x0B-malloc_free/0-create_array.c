9f48605bfe889edb9b3a7a75ecf017a48d07a993
Hatim Ousilmaati <hatim.ousilmaati@gmail.com>
2023-03-13 11:28:44 -0700

    Updated 0x0B-malloc_free

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,10 +1,11 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 /**
  * create_array - creates array or chars
  * @size: array size
  * @c: elems of array
- * Return: arr or NULL
+ * Return: arr or NULL or 1
  */
 char *create_array(unsigned int size, char c)
 {
@@ -18,9 +19,17 @@ char *create_array(unsigned int size, char c)
 	else
 	{
 		arr = malloc(size * sizeof(char));
-		for (i = 0 ; i < size ; i++)
+		if (arr == NULL)
 		{
-			arr[i] = c;
+			printf("Can't allocate %d bytes\n");
+			return (1);
+		}
+		else
+		{
+			for (i = 0 ; i < size ; i++)
+			{
+				arr[i] = c;
+			}
 		}
 		return (arr);
 	}
