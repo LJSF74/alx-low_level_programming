6b3bb48cba325090b55416c900d1332ec3a90eb1
Hatim Ousilmaati <hatim.ousilmaati@gmail.com>
2023-03-20 16:25:19 -0700

    0x0E-structures_typedef

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * init_dog - initializes a dog variable
+ * @d: dog struct
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
