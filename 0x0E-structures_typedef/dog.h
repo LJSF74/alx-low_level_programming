6ea1e4062d236ad9299f3a5296fba0dfb5257eb6
Hatim Ousilmaati <hatim.ousilmaati@gmail.com>
2023-03-20 16:20:22 -0700

    0x0E-structures_typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,15 @@
-struct dog {
+#ifndef DOG
+#define DOG
+/**
+ * struct dog - dog characteristics
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner name
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
 };
+#endif
