a0af18f9ef15a634e15c28b67adffc1901ff267f
Hatim Ousilmaati <hatim.ousilmaati@gmail.com>
2023-03-20 19:29:25 -0700

    0x0E-structures_typedef

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,13 +1,16 @@
 #include "dog.h"
 #include <stdlib.h>
 /**
- * free_dog: frees memory slots of dog var
+ * free_dog - frees memory slots of dog var
  * @d: dog variable
  * Return: void
  */
 void free_dog(dog_t *d)
 {
-	free((*d).name);
-	free((*d).owner);
-	free(d);
+	if(d)
+	{
+		free((*d).name);
+		free((*d).owner);
+		free(d);
+	}
 }
