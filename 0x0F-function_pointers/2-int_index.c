878b7cda84c8297629c594376b2910fb889c3ec5
Hatim Ousilmaati <hatim.ousilmaati@gmail.com>
2023-03-22 07:40:16 -0700

    Updated 0x0F-function_pointers

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,11 +14,17 @@ int int_index(int *array, int size, int (*cmp)(int))
 	{
 		return (-1);
 	}
-	for (i = 0 ; i < size ; i++)
+	if (array)
 	{
-		if ((*cmp)(array[i] == 1))
+		if (cmp)
 		{
-			return (i);
+			for (i = 0 ; i < size ; i++)
+			{
+				if ((*cmp)(array[i]) == 1)
+				{
+					return (i);
+				}
+			}
 		}
 	}
 	return (-1);
