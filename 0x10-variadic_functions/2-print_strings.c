b10271f936a11ea0cf4224969ef7c502f351b811
Hatim Ousilmaati <hatim.ousilmaati@gmail.com>
2023-03-23 10:40:37 -0700

    Updated 0x10-variadic_functions

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,23 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+/**
+ * print_strings - prints strings with separators
+ * @separator: separator
+ * @n: num of args
+ * Return: void
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list arg;
+	unsigned int i;
+
+	va_start(arg, n);
+	for (i = 0 ; i < n ; i++)
+	{
+		printf("%s", va_arg(arg, char *));
+		if (separator && i < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(arg);
+}
