3dbbc76b7be0032fbed4b1a89dddade6d72125b9
Hatim Ousilmaati <hatim.ousilmaati@gmail.com>
2023-03-23 10:24:10 -0700

    Updated 0x10-variadic_functions

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,22 @@
+#include "variadic_functions.h"
+/**
+ * sum_them_all - sums all the ints in arg
+ * @n: first int
+ * Return: s
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list arg;
+	int i;
+	int s = 0;
+
+	if (n == 0)
+		return (0);
+	va_start(arg, n);
+	for (i = 0 ; i < n ; i++)
+	{
+		s += va_arg(arg, int);
+	}
+	va_end(arg);
+	return (s);
+}
