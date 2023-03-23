0a4dd738c573d29133805f564bc4312de5a86466
Hatim Ousilmaati <hatim.ousilmaati@gmail.com>
2023-03-22 17:09:19 -0700

    Updated 0x0F-function_pointers

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -23,7 +23,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(100);
 	}
-	if (!((*get_op_func)(argv[2])(atoi(argv[1]), atoi(argv[3]))))
+	if (!((*get_op_func)(argv[2])(atoi(argv[1]), atoi(argv[3]))) || argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
