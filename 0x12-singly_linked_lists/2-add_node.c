d04b41923bc978143b6a70e59db16150ff389ec2
Hatim Ousilmaati <hatim.ousilmaati@gmail.com>
2023-03-30 10:09:15 -0700

    Updated 0x12-singly_linked_lists

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <string.h>
+#include <stdlib.h>
+/**
+ * add_node - adds elem to linked list
+ * @head: linked list
+ * @str: elem
+ * Return: p
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t **p = head;
+	list_t *n;
+	char *ch = strdup(str);
+
+	n = malloc(sizeof(list_t));
+	n->str = malloc(strlen(ch) + 1);
+	strcpy(n->str, ch);
+	n->len = strlen(ch);
+	n->next = NULL;
+	while(*p)
+	{
+		p = &(*p)->next;
+	}
+	*p = n;
+	return (*p);
+}
