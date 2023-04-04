bfe8e17c700ede1be9bf470f1dd8f941c53dc90b
Hatim Ousilmaati <hatim.ousilmaati@gmail.com>
2023-04-04 07:50:06 -0700

    Updated 0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,6 +22,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	for (i = 0 ; i < idx - 1 ; i++)
 	{
 		p = p->next;
+		if (p == NULL)
+		{
+			return (NULL);
+		}
 	}
 	new->next = p->next;
 	p->next = new;
