083f5ac5e33a5587338efae9893538c34b2d1920
Hatim Ousilmaati <hatim.ousilmaati@gmail.com>
2023-04-04 07:30:23 -0700

    Updated 0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,6 +1,9 @@
 #include "lists.h"
 /**
- *
+ * get_nodeint_at_index - gets value at specified index
+ * @head: list
+ * @index: index
+ * Return: p
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
@@ -10,6 +13,10 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	for (i = 0 ; i < index ; i++)
 	{
 		p = p->next;
+		if (p == NULL)
+		{
+			break;
+		}
 	}
 	return (p);
 }
